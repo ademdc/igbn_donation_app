08185affd5640ac146bab9482c301cfede30f84c
Adem Dinarević <ademdinarevic@gmail.com>
2025-10-24 21:44:03 +0200

    Set root

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,8 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "home#index"
+  
    resources :donations, only: [:create] do
     member do
       get :status
